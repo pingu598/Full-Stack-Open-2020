951871b433362c97e1d5426bda53854aead2a7a9
pingu598 <62209622+pingu598@users.noreply.github.com>
2020-05-07 17:33:02 +0300

    Add files via upload

---
+++ osa1/1.13/src/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+const RandomNum = () => {
+
+    return (
+        Math.floor(Math.random() * 2)     
+    )
+}
+
+const points = [ 0,0,0,0,0,0 ]
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const [votes, setVote] = useState(0)
+    
+
+    const SetAnec = () => {       
+        setSelected(RandomNum())  
+        setVote(points[selected])
+    }
+    const AddVote = () => {        
+        setVote(points[selected] + 1)
+        points[selected] += 1
+    }
+     
+   
+    return (
+        <div>                   
+            {props.anecdotes[selected]}
+            <p>has {points[selected]} votes </p>
+            <Button onClick={AddVote} text='vote' />  
+            <Button onClick={SetAnec} text='next anecdote' />  
+                
+        </div>
+    )
+}
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+    <App anecdotes={anecdotes} />,
+    document.getElementById('root')
+)
