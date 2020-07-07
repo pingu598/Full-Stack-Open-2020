b8b804d99d679fb1cf862daa5bc146c89dcf7533
pingu598 <62209622+pingu598@users.noreply.github.com>
2020-07-07 18:05:26 +0300

    Add files via upload

---
+++ osa2/2.19-20/src/services/persons.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+const delett = (id, oldObject) => {
+  const request = axios.delete(`${baseUrl}/${id}`, oldObject)
+  console.log(`${baseUrl}/${id}`, oldObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, update, delett }
